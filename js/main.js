657c716973c3834d1df02d683cc570bb3cc055c6
Михаил <240395@mail.ru>
2022-10-15 20:28:13 +0300

    Написал программу для создания массива из 25 сгенерированных объектов согласно ТЗ

--- js/main.js
+++ js/main.js
@@ -12,8 +12,8 @@ function getRandomPositiveNumber(a, b) {
 
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
-  const resaults = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(resaults);
+  const results = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(results);
 }
 
 getRandomPositiveNumber('keks', 100);
@@ -25,3 +25,83 @@ function checkStringLength(string, length) {
 }
 
 checkStringLength('', 140);
+
+
+const dataArray = [];
+const numberElemets = 25;
+
+
+const id = [];
+const fillId = function () {
+  for (let i = 1; i <= numberElemets; i++) {
+    id.push(i);
+  }
+};
+
+fillId();
+// console.log(id);
+
+const url = [];
+const fillUrl = function() {
+  for(let i = 1; i <= numberElemets; i++) {
+    url.push(`photos/${ i }.jpg`);
+  }
+};
+
+fillUrl();
+// console.log(url);
+
+
+const description = [
+  'Наступило бабье лето —',
+  'Дни прощального тепла.',
+  'Поздним солнцем отогрета,',
+  'В щелке муха ожила.',
+
+  'Солнце! Что на свете краше',
+  'После зябкого денька?..',
+  'Паутинок легких пряжа',
+  'Обвилась вокруг сучка.',
+
+  'Завтра хлынет дождик быстрый,',
+  'Тучей солнце заслоня.',
+  'Паутинкам серебристым',
+  'Жить осталось два-три дня.',
+
+  'Сжалься, осень! Дай нам света!',
+  'Защити от зимней тьмы!',
+  'Пожалей нас, бабье лето:',
+  'Паутинки эти — мы.',
+];
+
+const createObject = function () {
+  const j = getRandomPositiveNumber(0, id.length - 1);
+  //тут ввожу ещё одну случайную переменную k, чтобы не было совпадения id и url
+  const k = getRandomPositiveNumber(0, url.length - 1);
+  // console.log('j равно ' + j);
+  // console.log('k равно ' + j);
+  // console.log('массив до сплайса выглядит вот так: ' + id)
+  // console.log('длина массива до сплайса равна ' + id.length)
+  const object = {
+    id: id[j],
+    url: url[k],
+    likes: getRandomPositiveNumber(15, 200),
+    comments: getRandomPositiveNumber(0, 200),
+    description: description[getRandomPositiveNumber(0, description.length - 1)]
+  };
+  id.splice(j, 1);
+  url.splice(k, 1);
+  // console.log('массив после сплайса выглядит вот так: ' + id)
+  // console.log('длина массива после сплайса равна ' + id.length)
+  return object;
+};
+
+const createAnotherObjects = function () {
+  for (let i = 0; i <= numberElemets - 1; i++) {
+    dataArray.push(createObject());
+  }
+  return dataArray;
+};
+
+createAnotherObjects();
+// console.log(dataArray);
